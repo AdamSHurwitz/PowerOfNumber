521167d9cb13f390aa56cca071ecad7e7b8ee217
adamhurwitz <3Qszqx55!!>
2016-03-08 02:20:00 -0800

    refactor: add case for 9

--- src/main/java/javaclasses/Factorial.java
+++ src/main/java/javaclasses/Factorial.java
@@ -4,7 +4,7 @@ package javaclasses;
  * Created by adamhurwitz on 2/27/16.
  */
 public class Factorial {
-    static int n = 5;
+    static int n = 0;
 
     public static void main(String[] arg) {
         System.out.println("Factorial of " + n + " is " + factorial(n));
@@ -12,9 +12,13 @@ public class Factorial {
 
     public static int factorial(int n) {
         int s = n;
-        for (int x = n - 1; x > 0; x--) {
-            s = s * x;
+        if (n != 0) {
+            for (int x = n - 1; x > 0; x--) {
+                s = (s * x);
+            }
+            return s;
+        } else {
+            return 1;
         }
-        return s;
     }
 }
