c35007bf50bc16a307d18df2828a1c48df480cf2
adamhurwitz <3Qszqx55!!>
2016-03-08 02:01:01 -0800

    feat: updated factorial

--- src/main/java/javaclasses/Class.java
+++ src/main/java/javaclasses/Class.java
@@ -3,16 +3,19 @@ package javaclasses;
 /**
  * Created by adamhurwitz on 2/27/16.
  */
-public class Class {
-    static private final String LOG_TAG = Class.class.getSimpleName();
+public class Factorial {
 
-    static int v = 18;
+    static int n = 5;
 
     public static void main(String[] arg) {
-        System.out.println("Answer is " + method(v));
+        System.out.println("Factorial of " + n + " is " + factorial(n));
     }
 
-    public static int method(int v) {
-        return v;
+    public static int factorial(int n) {
+        int s = n;
+        for (int x = n - 1; x > 0 ; x--) {
+            s = s * x;
+        }
+        return s;
     }
 }
